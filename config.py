378d1e04380c64c8ac91f2559f42aa994357faac
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-07-29 03:56:13 +0100

    Code: refactoring

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:08164462713@localhost:5432/nigeruni'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aA#atinigerdb@atinigerdb.co5demze0mjs.eu-north-1.rds.amazonaws.com:5432/database_atiniger'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
