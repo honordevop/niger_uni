dc7128a390c9005a2d3ddbf90c0b9909400c96f3
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-11-30 13:54:26 +0100

    Code: implemented NEON db

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
