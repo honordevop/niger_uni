3521b1aebc0399ef4eff523dee878530664221b0
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2023-12-02 14:09:34 +0100

    Code: Added comment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-
+# Create App
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
